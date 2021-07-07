838321f2d8fc89f5624257059c3cb4cc58a4fe74
Gabriel Rodríguez <gabrielrf96@gmail.com>
2021-07-08 01:37:10 +0200

    Add utility script to easily increase version number

---
+++ scripts/inject-version.js
@@ -0,0 +1,16 @@
+const PACKAGE = require('../package.json');
+const replace = require('replace-in-file');
+
+try {
+    let changedFiles = replace.sync({
+        files: [
+            `${__dirname}/../README.md`,
+            `${__dirname}/../API.md`,
+        ],
+        from: /# HTML Artisan \*v([^*]*)\*/g,
+        to: `# HTML Artisan *v${PACKAGE.version}*`
+    });
+    console.log('Version injected successfully: ', changedFiles);
+} catch (error) {
+    console.error('Error occurred: ', error);
+}
